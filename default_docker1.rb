13147e06dd889c5fae9e8e4d8c8eefe78c6ea294
salmant <salman.taherizadeh@gmail.com>
2019-10-14 00:41:47 +0200

    Create default_docker1.rb

---
+++ default_docker1.rb
@@ -0,0 +1,27 @@
+# ------------------------------------------------------------------------
+# Author: Salman Taherizadeh - Jozef Stefan Institute (JSI)
+# This code is published under the Apache 2 license
+# ------------------------------------------------------------------------
+
+# Pull latest image of MongoDB
+docker_image 'mongo' do
+	tag 'latest'
+	action :pull
+end
+
+# Run the MongoDB container
+docker_container 'mongo' do
+repo 'mongo'
+tag 'latest'
+port '27017:27017'
+host_name 'ec2-44-12-91-174.eu-central-1.compute.amazonaws.com'
+extra_hosts ['ec2-44-12-91-174.eu-central-1.compute.amazonaws.com:44.12.91.174', 'ec2-20-60-100-120.eu-central-1.compute.amazonaws.com:20.60.100.120']
+command '--replSet rs0'
+detach true
+volume '/home/mongo-files/data:/data'
+end
+
+# Install the MongoDB client
+package "mongodb-clients" do
+action :install
+end
